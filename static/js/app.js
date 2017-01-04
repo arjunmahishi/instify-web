802ed98c7f2346743c907d126ddf7d8c37e7a0ff
arjunmahishi <arjun.mahishi@gmail.com>
2017-01-04 22:33:14 +0530

    Added firebase

---
+++ static/js/app.js
@@ -0,0 +1,9 @@
+var config = {
+	apiKey: "AIzaSyCR-bp7LI8JCPKYUjom5XP-8jTZZoR4N9g",
+	authDomain: "instify-62dda.firebaseapp.com",
+	databaseURL: "https://instify-62dda.firebaseio.com",
+	storageBucket: "instify-62dda.appspot.com",
+	messagingSenderId: "299280645644"
+};
+
+firebase.initializeApp(config);
