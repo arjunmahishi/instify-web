638f6da4d95abf9ba745852a05251c6654723664
arjunmahishi <arjun.mahishi@gmail.com>
2017-01-11 13:56:28 +0530

    Routed URL for the API

--- instify_web/urls.py
+++ instify_web/urls.py
@@ -1,8 +1,9 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from adminPanel import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.landing),
+    url(r'^polls/', include('API.urls'))
 ]
